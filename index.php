a70a5250eba6dd2ecf53b1441e5c7443f2c936cd
marckmannen <karstenmarck611@gmail.com>
2023-06-12 21:26:54 +0200

    added homepage

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="wrap">
+            <div class="title"><h1>Welcome to my portfolio!</h1></div>
+            <div class="subtitle">I am Karsten Marck, a beginning software developer, you can find some of my silly projects here :0</div>
+        </div>
+    </div>
+</body>
+</html>

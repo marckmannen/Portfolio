acef503f5502c3d245f68d1db8a0d7c9bad0652e
marckmannen <karstenmarck611@gmail.com>
2023-06-12 22:04:49 +0200

    some changes

--- index.php
+++ index.php
@@ -4,13 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bagel+Fat+One&family=Roboto+Mono&family=Teko:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
         <div class="wrap">
             <div class="title"><h1>Welcome to my portfolio!</h1></div>
-            <div class="subtitle">I am Karsten Marck, a beginning software developer, you can find some of my silly projects here :0</div>
+            <div class="subtitle">I am Karsten Marck, a starting-out software developer, you can find some of my silly projects here :)</div>
+        </div>
+        <div class="card">
+        <h2>Weather App</h2>
+            <img src="./img/weather-app.png" alt="Card Image" class="card-img">
+            <div class="content">
+                <p>This is my first ever project,</p>
+            </div>
         </div>
     </div>
 </body>
